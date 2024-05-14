9dc7c22d94391c2e6c34c16e402bc107f0c7d730
Miquel Avellaneda <mavellanedal2223@politecnics.barcelona>
2024-05-14 12:16:09 +0200

    Added the porfolio
    I added all the age to the repo

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  i18n: { 
+    defaultLocale: 'es', 
+    locales: ['es', 'en', 'ca'],
+      routing: {
+        prefixDefaultLocale: false
+      },
+    },
+});
